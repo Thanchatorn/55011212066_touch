7332bb19e452eeb86dfc3867d42dc75832ee45a5
Thanchatorn <touch.than@gmail.com>
2015-01-30 15:08:05 +0700

    userPass

--- userPass.playground/section-1.swift
+++ userPass.playground/section-1.swift
@@ -7,29 +7,41 @@ var str = "Hello, userPass"
 class userPass {
     var namePass: [String: String] = ["bob":"1234", "alice":"4321", "jack":"2468"]
     
-    func check(name:String)->String {
+    func check()->String {
         var str:String
         if namePass.isEmpty {
             str = "namePass dictionary is empty."
         }else{
             str = "namePass dictionary is not empty."
         }
-        return name
+        return str
     }
-
-    func add(name:String){
-        
-       namePass[name] = "5555"
-        
+    
+    func add(name:String, pass:String)->String {
+        if(namePass[name] == nil){
+            namePass[name] = pass
+            return "add complete \(namePass)"
+        }else{
+            return "cannot add \(namePass)"
+        }
+    }
+    
+    func update(name:String, pass:String) -> String {
+        if(namePass[name] != nil){
+            namePass[name] = pass
+            return "edit complete \(namePass)"
+        }else{
+            return "cannot edit \(namePass)"
+        }
     }
 
     func del(name: String){
-        let y = name
         namePass[name] = nil
     }
 }
 
 let edit = userPass()
-edit.check("bob")
-edit.add("joe")
+edit.check()
+edit.add("joe",pass:"123456")
+edit.update("joe",pass:"12")
 edit.del("joe")
