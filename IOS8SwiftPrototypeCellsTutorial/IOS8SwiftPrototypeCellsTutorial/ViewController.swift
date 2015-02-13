2958472c7b31780bbc9063003943ed1102687eb8
Thanchatorn <touch.than@gmail.com>
2015-02-13 15:07:32 +0700

    IOS8TableView

---
+++ IOS8SwiftPrototypeCellsTutorial/IOS8SwiftPrototypeCellsTutorial/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  IOS8SwiftPrototypeCellsTutorial
+//
+//  Created by iStudents on 2/13/15.
+//  Copyright (c) 2015 iStudents. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate {
+
+    let cellIdentifier = "CellIdentifier"
+    var tableData = ["Camera", "Music", "Gear"]
+    
+    @IBOutlet weak var tableVeiw: UITableView!
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    func tableView(UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableVeiw!.dequeueReusableCellWithIdentifier(self.cellIdentifier) as UITableViewCell
+        cell.textLabel!.text = self.tableData[indexPath.row]
+           var imageName = UIImage(named: tableData[indexPath.row])
+           cell.imageView!.image = imageName
+        return cell
+    }
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        let alert = UIAlertController(title: "Item selected", message: "You selected item \(indexPath.row)", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: {(alert: UIAlertAction!) in print("An alert of type \(alert.style.hashValue) was tapped!")
+        }))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.tableVeiw.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.cellIdentifier)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
